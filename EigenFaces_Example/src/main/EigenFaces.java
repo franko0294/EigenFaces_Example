fc52cf63f91a98ebfee7237aa91ed9387c258cb4
Francis <francis.berti@hotmail.co.uk>
2017-01-10 16:22:48 +0000

    Completed eigen method, now need to implement image2face

--- EigenFaces_Example/src/main/EigenFaces.java
+++ EigenFaces_Example/src/main/EigenFaces.java
@@ -3,25 +3,31 @@ package main;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
-import org.opencv.core.*;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class EigenFaces {
-
-	private Mat eigenvalues;
-	private Mat eigenvectors;
+	
+	private static boolean newEigens;
+	private static Mat eig_vec;
+	private static Mat eig_val;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		learn("C:/Users/franc/OneDrive/Documents/University/Year 3/Final Year Project/pics/jpg");
+		//learn("C:/Users/franc/OneDrive/Documents/University/Year 3/Final Year Project/pics/jpg");
+		learn("C:/Users/Francis/git/EigenFaces_Example/EigenFaces_Example/pics");
 	}
 	
 	private static void learn(String pathname)
@@ -30,20 +36,28 @@ public class EigenFaces {
 		
 		Mat imagesMat = new Mat();
 		
-		System.out.println("Reading images");
+		//System.out.println("Reading images");
 		//ArrayList<Mat> images = read_images(pathname);
 		imagesMat = read_images_mat(pathname);
 		
 		
-		System.out.println("Averaging images");
+		//System.out.println("Averaging images");
 		//Mat avg = mean_image(images);
 		Mat avg = mean_image_mat(imagesMat);
 		
 		Mat evalues = new Mat();
 		Mat evectors = new Mat();
 		
-		System.out.println("Eigening images");
+		//System.out.println("Eigening images");
+		eigen_images(imagesMat);
 		//Core.eigen(avg, evalues, evectors);
+		
+		for(int i = 0; i < imagesMat.rows(); i++)
+		{
+			
+		}
+		
+		System.out.println("Done");
 		
 	}
 
@@ -82,15 +96,17 @@ public class EigenFaces {
 	
 	private static Mat read_images_mat(String pathname)
 	{
-		ArrayList<Mat> images = new ArrayList<>();
-		Mat imagesMat;
-		BufferedImage img = null;
+		Mat imagesMat = null;
+		boolean imagesMatInit = false;
 		
 		File folder = new File(pathname);
 		File[] files = folder.listFiles();
 		
+		int row = 0;
+		
 		for (File file : files) {
-			System.out.println(file.toString());
+			//System.out.println(file.toString());
+			
 			Mat image = Imgcodecs.imread(file.toString());
 			
 			if(image.channels() > 1)
@@ -98,53 +114,37 @@ public class EigenFaces {
 				Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);	
 			}
 			
+			//output_matrix(image, (file + ".txt"));
+			
+			if(!imagesMatInit)
+			{
+				imagesMat = Mat.zeros(files.length, image.rows() * image.cols(), CvType.CV_64FC1);
+				imagesMatInit = true;
+				
+				//System.out.println("About to output");
+				
+				//output_matrix(imagesMat, "zeroedimagesmat.txt");
+			}
+			
 			//System.out.println(image.dump());
 			
 			image.convertTo(image, CvType.CV_64FC1);
 			
-			images.add(image);
-			/*
-			try {
-				//img = ImageIO.read(file);
-				//byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-				
-				Mat image = Imgcodecs.imread(file.toString());
-				
-				//Mat img_final = new Mat();
-				
-				//System.out.println("Putting pixels");
-				//img_final.put(0, 0, pixels);
-				if(image.channels() > 1)
-				{
-					Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);	
-				}
-				
-				image.convertTo(image, CvType.CV_8UC1);
-				
-				images.add(image);
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-		}
-		
-		imagesMat = Mat.zeros(images.size(), images.get(0).rows() * images.get(0).cols(), CvType.CV_64FC1);
-		
-		System.out.println(images.get(0).dump());
-		
-		for(int i = 0; i < images.size(); i++)
-		{
-			images.get(i).reshape((images.get(i).rows() * images.get(i).cols()), 1);
+			double[] data = new double[(int) (image.rows() * image.cols())];
 			
-			byte[] data = new byte[(int) (images.get(i).rows() * images.get(i).cols())];
+			//System.out.println(data.length);
 			
-			images.get(i).get(0, 0, data);
+			image.get(0, 0, data);
 			
-			//System.out.println(images.get(i).dump());
+			imagesMat.put(row, 0, data);
 			
-			imagesMat.put(i, 0, data);
-			//System.out.println(imagesMat.submat(0, 5, 0, 500).dump());
+			//output_matrix(imagesMat, "imagesMat.txt");
+			
+			
+			//images.add(image);
+			row++;
+			
+			//break;
 		}
 		
 		return imagesMat;
@@ -187,13 +187,164 @@ public class EigenFaces {
 			avgMat.put(0, i, columnAvg);
 		}
 		
+		//output_matrix(avgMat, "avgmat.txt");
 		//System.out.println(avgMat.dump());
 		
 		return avgMat;
 	}
 	
-	private void eigen_images(ArrayList<Mat> images)
+	private static void eigen_images(Mat images)
 	{
+		newEigens = false;
+		Mat copy = new Mat(images.rows(), images.cols(), images.type());
+		
+		images.copyTo(copy);
+		
+		int x = copy.rows();
+		int y = copy.cols();
+		
+		copy = subtract_mean(copy);
+		
+		snapshot_eig(copy);
+		
+		
+		//output_matrix(copy, "newcopy.txt");
+	}
+	
+	private static Mat subtract_mean(Mat images)
+	{
+		Mat copy = new Mat(images.rows(), images.cols(), images.type());
+		
+		int rows = copy.rows();
+		int cols = copy.cols();
+		
+		Mat avg = mean_image_mat(images);
+		//output_matrix(avg, "avg_eigen.txt");
+		for(int i = 0; i < rows; i++)
+		{
+			Mat row = new Mat();
+			Core.subtract(images.row(i), avg, row);
+			
+			double[] data = new double[(int) (rows * cols)];
+			
+			//System.out.println(data.length);
+			
+			row.get(0, 0, data);
+			
+			copy.put(i, 0, data);
+		}
+		
+		//output_matrix(copy, "subtracted_mean.txt");
+		
+		return copy;
+	}
+	
+	private static void snapshot_eig(Mat images)
+	{
+		Mat snap_vec = new Mat();
+		Mat snap_val = new Mat();
+		
+		Mat copy = new Mat();
+		Mat covar = new Mat();
+		Mat mean = new Mat();
+		
+		int rows = images.rows();
+		int cols = images.cols();
+		
+		copy = images.t();
+		Core.calcCovarMatrix(copy, covar, mean, Core.COVAR_COLS, images.type());
+		
+		output_matrix(covar, "covar.txt");
+		
+		Core.eigen(covar, snap_val, snap_vec);
+		
+		output_matrix(snap_val, "snap_val.txt");
+		output_matrix(snap_vec, "snap_vec.txt");
+		
+		eig_vec = Mat.zeros(rows, cols, images.type());
+		Mat eig_temp = new Mat(rows, cols, images.type());
+		
+		for(int i = 0; i < rows; i++)
+		{
+			Mat row = eig_vec.row(i);
+			for(int j = 0; j < rows; j++)
+			{
+				Mat temp = new Mat();
+				Core.multiply(images.row(j), new Scalar(snap_vec.get(j, i)), temp);
+				Core.add(row, temp, row);
+			}
+			
+			double[] data = new double[(int) (rows * cols)];
+			
+			//System.out.println(data.length);
+			
+			row.get(0, 0, data);
+			
+			eig_temp.put(i, 0, data);
+		}
+		
+		eig_val = snap_val.diag();
+		
+		eig_vec = normalise(eig_temp);
+		
+		newEigens = true;
+	}
+	
+	private static Mat normalise(Mat matrix)
+	{
+		
+		int rows = matrix.rows();
+		int cols = matrix.cols();
+		Mat norm = new Mat(rows, cols, matrix.type());
+		
+		for(int i = 0; i < rows; i++)
+		{
+			Mat row = matrix.row(i);
+			
+			//System.out.println(row.dump());
+			
+			Mat normRow = new Mat();
+			
+			Core.normalize(row, normRow);
+			
+			//System.out.println("normRow:\n" + normRow.dump());
+			
+			Core.divide(row, normRow, row);
+			
+			//append row into new matrix
+			double[] data = new double[(int) (rows * cols)];
+			
+			row.get(0, 0, data);
+			
+			norm.put(i, 0, data);
+			
+			//output_matrix(norm, "norm_" + i +".txt");
+		}
+		System.out.println(norm);
+		//output_matrix(norm, "norm.txt");
+		
+		return norm;
+	}
+	
+	public static void output_matrix(Mat matrix, String filename)
+	{
+		File output = new File(filename);
+		
+		String outputstring = matrix.dump();
+		try {
+			System.out.println("Writing file");
+			FileWriter outputstream = new FileWriter(output);
+			
+			outputstream.write(outputstring);
+			
+			outputstream.close();
+			
+			System.out.println("Output completed");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
